417bc4e600b4a16010072db83fe68477fc761378
byeongminn <sekn1997@naver.com>
2024-06-05 20:05:29 +0900

    test: Button 컴포넌트의 사양 2번에 대한 테스트 코드 작성

--- src/Components/Button/index.test.tsx
+++ src/Components/Button/index.test.tsx
@@ -1,13 +1,14 @@
 /*
   <Button 컴포넌트 사양>
   1. 공통 컴포넌트이므로 부모 컴포넌트에서 버튼의 라벨을 결정할 수 있다.
-  2. 기본적으로 파란색 계열의 배경색(#304ff3)과 마우스 호버 색상(#1e40ff)이 화면에 표시된다. 하지만, 필요하면 배경색과 마우스 호버 색상을 부모 컴포넌트에서 변경할 수 있다.
+  2. 기본적으로 파란색 계열의 배경색(#304ffe)과 마우스 호버 색상(#1e40ff)이 화면에 표시된다. 하지만, 필요하면 배경색과 마우스 호버 색상을 부모 컴포넌트에서 변경할 수 있다.
   3. 버튼을 누르고 있으면 버튼 안쪽에 box-shadow를 표시한다. (box-shadow: inset 5px 5px 10px rgba(0, 0, 0, 0.2))
   4. 버튼을 클릭할 수 있으며, 클릭하였을 때 동작은 부모 컴포넌트가 결정한다.
 */
 
 import { render, screen } from '@testing-library/react';
 import { Button } from '.';
+import 'jest-styled-components';
 
 describe('<Button />', () => {
   it('renders component correctly', () => {
@@ -15,5 +16,30 @@ describe('<Button />', () => {
 
     const label = screen.getByText('추가');
     expect(label).toBeInTheDocument();
+
+    const parent = label.parentElement;
+    expect(parent).toHaveStyleRule('background-color', '#304ffe');
+    expect(parent).toHaveStyleRule('background-color', '#1e40ff', {
+      modifier: ':hover',
+    });
+  });
+
+  it('changes backgroundColor and hoverBackgroundColor Props', () => {
+    const backgroundColor = '#304ffe';
+    const hoverBackgroundColor = '#1e40ff';
+
+    render(
+      <Button
+        label="추가"
+        backgroundColor={backgroundColor}
+        hoverBackgroundColor={hoverBackgroundColor}
+      />,
+    );
+
+    const parent = screen.getByText('추가').parentElement;
+    expect(parent).toHaveStyleRule('background-color', backgroundColor);
+    expect(parent).toHaveStyleRule('background-color', hoverBackgroundColor, {
+      modifier: ':hover',
+    });
   });
 });
