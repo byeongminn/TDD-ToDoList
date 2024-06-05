37b91e3bda8e27fb213ce7db4296ca7cb110a2f9
byeongminn <sekn1997@naver.com>
2024-06-05 19:49:19 +0900

    test: Button 컴포넌트에 대한 사양 작성

---
+++ src/Components/Button/index.test.tsx
@@ -0,0 +1,10 @@
+/*
+  <Button 컴포넌트 사양>
+  1. 공통 컴포넌트이므로 부모 컴포넌트에서 버튼의 라벨을 결정할 수 있다.
+  2. 버튼을 누르고 있으면 버튼 안쪽에 box-shadow를 표시한다. (box-shadow: inset 5px 5px 10px rgba(0, 0, 0, 0.2))
+  3. 버튼을 클릭할 수 있으며, 클릭하였을 때 동작은 부모 컴포넌트가 결정한다.
+*/
+
+describe('<Button />', () => {
+  it('renders component correctly', () => {});
+});
