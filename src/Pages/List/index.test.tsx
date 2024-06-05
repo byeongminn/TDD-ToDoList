48adaa830c95dcf3864485a178314f20e412f775
byeongminn <sekn1997@naver.com>
2024-06-05 20:53:42 +0900

    test: <List /> 사양 1번에 대한 테스트 코드 작성

--- src/Pages/List/index.test.tsx
+++ src/Pages/List/index.test.tsx
@@ -7,6 +7,21 @@
   5. 추가 버튼을 누르면 할 일 추가 페이지('/add')로 이동한다.
 */
 
+import { render, screen } from '@testing-library/react';
+
 describe('<List />', () => {
-  it('renders component correctly', () => {});
+  it('renders component correctly', () => {
+    localStorage.setItem('ToDoList', '["ToDo 1", "ToDo 2", "ToDo 3"]');
+
+    render(<List />);
+
+    const toDoItem1 = screen.getByText('ToDo 1');
+    expect(toDoItem1).toBeInTheDocument();
+
+    const toDoItem2 = screen.getByText('ToDo 2');
+    expect(toDoItem2).toBeInTheDocument();
+
+    const toDoItem3 = screen.getByText('ToDo 3');
+    expect(toDoItem3).toBeInTheDocument();
+  });
 });
