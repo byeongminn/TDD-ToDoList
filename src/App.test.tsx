abfe2fabd42a9a0368568c6be41fe01f958797bf
byeongminn <sekn1997@naver.com>
2024-06-05 19:34:56 +0900

    test: <App /> 테스트 코드 작성

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,14 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
+import { MemoryRouter } from 'react-router-dom';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(
+    <MemoryRouter initialEntries={['/']}>
+      <App />
+    </MemoryRouter>,
+  );
+
+  const linkElement = screen.getByText('할 일 목록');
+  expect(linkElement).toBeInTheDocument();
 });
