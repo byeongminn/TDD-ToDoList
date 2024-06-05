9699ab7e0e144c8bd97d736c7a4dbb8c12262924
byeongminn <sekn1997@naver.com>
2024-06-05 19:38:17 +0900

    design: App UI 수정

--- src/App.tsx
+++ src/App.tsx
@@ -11,4 +11,11 @@ function App() {
 
 export default App;
 
-const Container = styled.div``;
+const Container = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #eeeeee;
+`;
