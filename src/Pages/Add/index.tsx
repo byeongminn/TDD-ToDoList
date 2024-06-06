5e19a2ee8b38d2383432c8b7258605772122f766
byeongminn <sekn1997@naver.com>
2024-06-06 15:05:48 +0900

    feat: <Add /> 사양 2번 구현

--- src/Pages/Add/index.tsx
+++ src/Pages/Add/index.tsx
@@ -1,9 +1,11 @@
+import { Button } from 'Components';
 import styled from 'styled-components';
 
 export const Add = () => {
   return (
     <Container>
       <Input placeholder="할 일을 입력해 주세요." />
+      <Button label="추가" />
     </Container>
   );
 };
