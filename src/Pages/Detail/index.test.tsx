9b08a1263d5e7009203690cb36908d7827e54f72
byeongminn <sekn1997@naver.com>
2024-06-06 15:41:19 +0900

    test: <Detail />에 대한 사양 작성

---
+++ src/Pages/Detail/index.test.tsx
@@ -0,0 +1,12 @@
+/* 
+  <Detail 페이지 컴포넌트 사양>
+  1. URL을 통해 할 일 목록에서 유저가 클릭한 할 일의 index 값을 전달받는다. (ex > /detail/2);
+  2. URL을 통해 전달받은 index를 사용하여 localStorage에 저장된 할 일 목록에서 해당 할 일을 가져와 표시한다.
+  3. 전달받은 매개변수에 해당하는 할 일이 없는 경우 NotFound 페이지('/NotFound')로 리다이렉트(Redirect) 시킨다.
+  4. 현재 보이는 할 일을 삭제할 수 있는 삭제 버튼이 있다.
+  5. 삭제 버튼을 클릭하면 현재 보이는 할 일을 삭제하고, 할 일 목록 페이지('/')로 이동한다.
+*/
+
+describe('<Detail />', () => {
+  it('renders component correctly', () => {});
+});
