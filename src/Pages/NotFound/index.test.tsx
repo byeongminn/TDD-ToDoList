fcf081a2f7cd3e5c677fca7a071731850090821d
byeongminn <sekn1997@naver.com>
2024-06-06 18:01:42 +0900

    test: <NotFound /> 사양 1번에 대한 테스트 코드 작성

--- src/Pages/NotFound/index.test.tsx
+++ src/Pages/NotFound/index.test.tsx
@@ -3,6 +3,13 @@
   1. 화면에 "Not Found"라는 글자를 표시한다.
 */
 
+import { render, screen } from '@testing-library/react';
+
 describe('<NotFound />', () => {
-  it('renders component correctly', () => {});
+  it('renders component correctly', () => {
+    render(<NotFound />);
+
+    const message = screen.getByText('Not Found');
+    expect(message).toBeInTheDocument();
+  });
 });
